633c33063b17835d524639e2df44fe466229d22a
Vlad Ra <vlad19@gmail.com>
2024-12-11 21:09:06 +0000

    work filtering

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const authors = defineCollection({
   type: 'data',
   schema: ({ image }) => z.object({
     order: z.number().optional(),
+    isFeatured: z.boolean().optional(),
     name: z.string(),
     workImg: z.union([image(), z.string()]).optional(),
     caseURL: z.union([z.string().url(), z.string()]).optional(),
